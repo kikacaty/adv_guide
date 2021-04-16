e5c045ab6102fd878cb28edf0ac9a469e82af854
xiaochaowei <chaoweixiao11@gmail.com>
2021-04-16 09:50:45 -0400

    update utils

--- train_moco.sh
+++ train_moco.sh
@@ -3,4 +3,5 @@ python main_moco.py \
   --lr 0.015 \
   --batch-size 128 \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
-  /dali
+  /dali \
+  2>&1 | tee training.log
